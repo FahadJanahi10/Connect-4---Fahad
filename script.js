fe40260581e1b422c31a21aca10c640c1e1a6aed
FahadJanahi10 <fahad.a.janahi@gmail.com>
2022-10-03 15:52:25 +0300

    bit of updates

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// const firstPlayer = "red";
+// const secondPlayer = "yellow";
+// const currentPlayer = firstPlayer;
+// const gameOn = false;
+// const columns = 7;
+// const rows = 6;
+
+// let box1 = document.querySelector('.one')
+// console.log(box1)
+// let img = document.createElement('img')
+// img.src = "./images/green_circle.png"
+// img.style.height = '90%'
+// img.style.width = '90%'
+// box1.appendChild(img)
