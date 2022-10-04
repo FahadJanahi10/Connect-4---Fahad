f816640631a62a09a34d27c433ea94b47ddeb9e3
FahadJanahi10 <fahad.a.janahi@gmail.com>
2022-10-04 09:25:51 +0300

    started over, used a loop to create the divs, might be simpler to code, fingers crossed

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+const grid = document.querySelector(".container");
+
+for(let i = 0; i < 42; i++){
+    let div = document.createElement("div");
+    div.className = "cell";
+    grid.appendChild(div);
+
+    div.onmouseenter =  () => {
+        console.log(i);
+    }
+}
+
+
+// let color = ();
+// color[-1] = "blue";
+// color[1] = "red";
+// let count = 0
+
+// $("#grid").each(function(){
+//     $(this).attribute("id", count);
+//     $(this).click(function(){
+//         if(isValid($(this.attribute("id"))){
+//             $(this).css("backGroundColor", color[currentPlayer]});
+            
+//     })
+// })
+
+
+
+// for(let i = 0; i < cells.length; i++){
+//     cells[i].onclick = () => {
+//         if(currentPlayer == 1){
+//             cells[i].classList.add("occupied");
+//             cells[i].classList.add("playerOne")
+//             currentPlayer = 2
+//             display.innerHTML = currentPlayer
+//         }
+//     }
+// }
+
 
 // const firstPlayer = "red";
 // const secondPlayer = "yellow";
